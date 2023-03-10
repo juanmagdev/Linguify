dbc68a2418a42b540bef8a6b35e43222958004f5
juanmagdev <me@juanmagd.dev>
2023-03-10 15:14:23 +0100

    generated words working

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
+import { useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  const [topic, setTopic] = useState('');
+  const [relatedWords, setRelatedWords] = useState([]);
+  
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch(`https://api.datamuse.com/words?rel_trg=${topic}`);
+    const data = await response.json();
+    const len = 10;
+    const relatedWordsArr = data.slice(0, len).map((item) => item.word);
+    setRelatedWords(relatedWordsArr);
+    
+    console.log(data);
+    console.log(topic);
+    console.log(relatedWords);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <h1>Linguify</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="text">Topic</label>
+        <input 
+          id="topic" 
+          type="text" 
+          value={topic} 
+          onChange={(event) => setTopic(event.target.value)} 
+        />
+
+        <button>Generate</button>
+      </form>
+
+      {relatedWords.length > 0 && (
+        <ul>
+          {relatedWords.map((relatedWord) => (
+            <li key={relatedWord}>{relatedWord}</li>
+          ))}
+        </ul>
+      )}
+    </>
+
+
   )
 }
 
